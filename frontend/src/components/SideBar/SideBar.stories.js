28ef9d40abaa0b2922864a14d322072d4fa10575
J190_정현구 <jungheun99@naver.com>
2021-11-02 16:07:17 +0900

    test: 사이드바 스트리머 리스트 storybook 추가

---
+++ frontend/src/components/SideBar/SideBar.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SideBar from './index.jsx';
+
+export default {
+  component: SideBar,
+  title: 'SideBar',
+};
+
+const Template = () => <SideBar/>;
+
+export const Default = Template.bind({});
+Default.args = {};
