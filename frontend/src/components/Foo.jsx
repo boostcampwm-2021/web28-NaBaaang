719667e718f68f45edf55948d8d9f24618a11134
Jaeuk Lim <applewodnr@naver.com>
2021-11-02 22:57:43 +0900

    chore: global font 다운 및 적용

--- frontend/src/components/Foo.jsx
+++ frontend/src/components/Foo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 const StyledFoo = styled.div`
+    font-family: 'notoSansLight';
     ${({ theme }) =>
         css`
             background-color: ${theme.color.primary};
