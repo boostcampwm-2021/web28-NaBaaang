1d3bd714a60667576d20d7a2f59a1a06201ee107
J190_정현구 <jungheun99@naver.com>
2021-11-02 18:06:15 +0900

    test: Channel Dialog storybook 추가

---
+++ frontend/src/components/ChannelDialog/ChannelDialog.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ChannelDialog from './index.jsx';
+
+export default {
+    title: 'ChannelDialog',
+    component: ChannelDialog,
+};
+
+const Template = () => <ChannelDialog />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
