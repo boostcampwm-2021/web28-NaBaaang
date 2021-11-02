1f03a2eb48091adef99f4a192222b9b538b5cf38
Jaeuk Lim <applewodnr@naver.com>
2021-11-02 18:15:12 +0900

    feat: slot 속성 적용

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -11,12 +11,31 @@ const boxStyle = {
     width: 400,
     height: 400,
     padding: 5,
-    borderRadius: 15,
+    borderRadius: 5,
     backgroundColor: '#fff',
     boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
 };
 
-export default function StyeldModal() {
+const headerStyle = {
+    marginBottom: 'auto',
+};
+
+const slotStyle = {
+    marginBottom: 'auto',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+};
+
+const buttonBoxStyle = {
+    marginTop: 'auto',
+};
+
+export default function StyeldModal({ title = true, isButton = false, slot }) {
     const [isOpen, setIsOpen] = useState(true);
 
     const handeModalClose = () => {
@@ -26,13 +45,22 @@ export default function StyeldModal() {
     return (
         <Modal open={isOpen} sx={modalStyle}>
             <Box sx={boxStyle}>
-                <Typography variant="h5" align="center">
-                    NaBaang
-                </Typography>
+                {title && (
+                    <Typography variant="h5" align="center" sx={headerStyle}>
+                        NaBaang
+                    </Typography>
+                )}
 
-                <Button variant="contained" onClick={handeModalClose}>
-                    취소
-                </Button>
+                {slot && <Box sx={slotStyle}>{slot}</Box>}
+
+                {isButton && (
+                    <Box sx={buttonBoxStyle}>
+                        <Button variant="contained" onClick={handeModalClose}>
+                            Cancle
+                        </Button>
+                        <Button variant="contained">OK</Button>
+                    </Box>
+                )}
             </Box>
         </Modal>
     );
--- frontend/src/components/Modal/Modal.stories.jsx
+++ frontend/src/components/Modal/Modal.stories.jsx
@@ -2,4 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Modal from './Modal';
 
-storiesOf('Modal', module).add('Show', () => <Modal />);
+function Foo() {
+    return <div>TEST!</div>;
+}
+
+storiesOf('Modal', module)
+    .add('버튼 X, 자식 X', () => <Modal />)
+    .add('버튼 O, 자식 X', () => <Modal isButton />)
+    .add('버튼 X, 자식 O', () => <Modal slot={<Foo />} />)
+    .add('버튼 O, 자식 O', () => <Modal slot={<Foo />} isButton />);
