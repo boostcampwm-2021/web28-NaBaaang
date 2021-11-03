c7122113f08a09370ed203eb3cd3f6454684c343
jihoho <jihoho4589@gmail.com>
2021-11-03 23:39:57 +0900

    fix: foo 컴포넌트 제거

--- frontend/src/components/Foo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import LiveCollection from './LiveCollection/LiveCollection';
-
-export default function foo() {
-    return <LiveCollection />;
-}
--- frontend/src/components/Foo.stories.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Foo from '@/components/Foo';
-
-storiesOf('TEST', module).add('TEST1', () =>  <Foo title="배고파요..." />);
