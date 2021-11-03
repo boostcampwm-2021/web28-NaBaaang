471f6f9539b4d41979ff09b50a9b8184d0c5a77a
Jaeuk Lim <applewodnr@naver.com>
2021-11-03 19:52:31 +0900

    feat: add donation item list component

--- frontend/src/components/DonationModal/DonationModal.jsx
+++ frontend/src/components/DonationModal/DonationModal.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import Modal from '@/components/Modal';
-import bit1Icon from '@/assets/images/bit-1.png';
-
-import DonationItem from './DonationItem';
+import DonationItemList from './DonationItemList';
 
 export default function DonationModal() {
     return (
         <Modal open showButton onSuccessText="전송" onCancleText="취소">
             <Title>비트를 선택해주세요</Title>
-            <DonationItem imgSrc={bit1Icon} value={1} />
+            <DonationItemList />
         </Modal>
     );
 }
