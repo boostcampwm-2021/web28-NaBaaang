7062c960203135dcef1b5e5ef9e8340308f03ddc
J190_정현구 <jungheun99@naver.com>
2021-11-04 11:00:50 +0900

    feat: add empty main page layout

---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainPage() {
+    return <div>main</div>;
+}
+
+export default MainPage;
---
+++ frontend/src/pages/MainPage/MainPage.stories.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import MainPage from '@/pages/MainPage';
+
+storiesOf('MainPage', module).add('default', () => <MainPage />);
