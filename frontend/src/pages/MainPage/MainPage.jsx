167795647650d946e484b109cd678e5638aef984
J190_정현구 <jungheun99@naver.com>
2021-11-04 11:18:02 +0900

    style: apply main page layout position

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+import { flexMixin } from '@/styles/mixins';
+
 function MainPage() {
-    return <div>main</div>;
+    return (
+        <MainLayout>
+            <Header>Header</Header>
+            <Body>
+                <SideBar>SideBar</SideBar>
+                <MainSection>MainSection</MainSection>
+            </Body>
+        </MainLayout>
+    );
 }
+
+const MainLayout = styled.main`
+    ${flexMixin('column')}
+`;
+
+const Header = styled.header`
+    height: 100px;
+    ${({ theme }) => `background-color: ${theme.color.primary};`}
+`;
+
+const Body = styled.section`
+    ${flexMixin('row')}
+`;
+
+const SideBar = styled.aside`
+    width: 10%;
+    ${({ theme }) => `background-color: ${theme.color.gray1};`}
+`;
+
+const MainSection = styled.section`
+    width: 90%;
+    ${({ theme }) => `background-color: ${theme.color.gray2};`}
+`;
 
 export default MainPage;
