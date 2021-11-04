eda2ed80114e9c347bce268c2eab0878a19c310c
JeongwonShin <jennyshin106@gmail.com>
2021-11-04 11:48:08 +0900

    fix: apply flexMixin & fontMixin to LiveList component

--- frontend/src/components/LiveCollection/LiveList.jsx
+++ frontend/src/components/LiveCollection/LiveList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { flexMixin, fontMixin } from '@/styles/mixins';
 
 import LiveSlider from './LiveSlider';
 
@@ -14,17 +15,12 @@ function LiveList({ category, liveList }) {
 
 const LiveListWrapper = styled.div`
     position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
+    ${flexMixin('column', 'center', 'flex-start')}
     margin-bottom: 1.5rem;
 `;
 
 const LiveListHeader = styled.span`
-    font-family: 'notoSansBold';
-    color: ${({ theme }) => theme.color.black};
-    font-size: 2em;
-    font-weight: bold;
+    ${fontMixin('2em', '1em', 'notoSansBold')}
 `;
 
 export default LiveList;
