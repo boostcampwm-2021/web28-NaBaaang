ced3e9354818fec6a2701820a4870bb293b0d6ab
Jaeuk Lim <applewodnr@naver.com>
2021-11-05 23:44:47 +0900

    refactor: 공용 Avatar component로 변경

--- frontend/src/components/Channel/ChannelDetail/ChannelDetail.jsx
+++ frontend/src/components/Channel/ChannelDetail/ChannelDetail.jsx
@@ -3,29 +3,31 @@ import styled, { css } from 'styled-components';
 import { borderBoxMixin, flexMixin, fontMixin } from '@/styles/mixins';
 import Button from '@/components/Common/Button';
 import Card from '@/components/Common/Card';
-import Avatar from './Avatar';
+import Avatar from '@/components/Common/Avatar';
 
 function ChannelDetails({ channelInfo }) {
     return (
         <Card
             width="100%"
             height="100%"
-            justify="flex-start"
-            alignItems="center"
             direction="row"
+            justify="fles-start"
+            alignItems="center"
         >
             <StyledRow>
-                <Avatar imageSrc={channelInfo.streamer.imageSrc} />
+                <Avatar src={channelInfo.streamer.imageSrc} size="large" />
             </StyledRow>
 
             <StyledRow marginLeft="20px">
                 <StyledColumn>
-                    <StyledName>{channelInfo.streamer.nickname}</StyledName>
+                    <StyledNickName>
+                        {channelInfo.streamer.nickname}
+                    </StyledNickName>
                     <StyledTitle>{channelInfo.title}</StyledTitle>
                     <StyledHashTag>#{channelInfo.category}</StyledHashTag>
                 </StyledColumn>
             </StyledRow>
-            <StyledRow marginLeft="auto">
+            <StyledRow marginLeft="auto" alignSelf="flex-start">
                 <StyledColumn>
                     <Button color="success" text="팔로우" size="medium" />
                     <StyledViews>시청자 수 12000</StyledViews>
@@ -36,21 +38,17 @@ function ChannelDetails({ channelInfo }) {
 }
 
 const StyledRow = styled.div`
-    ${({ marginLeft }) =>
+    ${({ alignSelf, marginLeft }) =>
         css`
+            alignSelf: ${alignSelf};
             margin-left: ${marginLeft};
         `}
-    align-self: flex-start;
 `;
 const StyledColumn = styled.div`
     ${flexMixin('column', 'flex-start', 'flex-start')}
-    ${({ marginLeft }) =>
-        css`
-            margin-left: ${marginLeft};
-        `}
 `;
-const StyledName = styled.div`
-    ${fontMixin('2em', '2em', 'notoSansBold')}
+const StyledNickName = styled.h3`
+    ${fontMixin('2em', '', 'notoSansBold')}
 `;
 
 const StyledTitle = styled.div`
