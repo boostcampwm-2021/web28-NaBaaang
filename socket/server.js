a3ee2f501ed9f0c9da0e02caf80c63336343ed1c
JeongwonShin <jennyshin106@gmail.com>
2021-11-09 17:10:53 +0900

    refactor: separate chat-related handlers in chatHandler.js

--- socket/server.js
+++ socket/server.js
@@ -1,5 +1,6 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import chatHandler from "./chatHandler.js";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
@@ -7,22 +8,11 @@ const io = new Server(httpServer, {
 });
 const port = 5000;
 
-io.on("connection", socket => {
-  console.log("[server] on connection");
-  socket.emit("greeting-from-server", { greeting: "Hello Client" });
-  socket.on("greeting-from-client", message => {
-    console.log(`${message} from ${socket.roomId}`);
-  });
+const onConnection = socket => {
+  chatHandler(io, socket);
+};
 
-  socket.on("join", ({ roomId }) => {
-    socket.roomId = roomId;
-    socket.join(roomId);
-    io.to(roomId).emit("after-join", "anonymous entered the room");
-  });
-  socket.on("disconnect", () => {
-    console.log(`${socket} socket disconnected`);
-  });
-});
+io.on("connection", onConnection);
 
 httpServer.listen(port, () =>
   console.log(`Socket.io started on PORT: ${port}`)
