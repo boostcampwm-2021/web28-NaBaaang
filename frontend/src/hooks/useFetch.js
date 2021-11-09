253ed2356b484ab7cefbeb36bd59553a483b33a0
Jaeuk Lim <applewodnr@naver.com>
2021-11-09 21:51:04 +0900

    feat: useFetch custom hook 생성

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export default function useFetch(url, option) {
+    const [data, setData] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(url, option);
+            const json = await response.json();
+            setData(json);
+        } catch (err) {
+            setError(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return { data, error, loading };
+}
