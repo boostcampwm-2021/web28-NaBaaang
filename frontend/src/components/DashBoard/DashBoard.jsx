25da9f5a73a4811ef31a7a15d1b90cfc7a1cbc2f
Jaeuk Lim <applewodnr@naver.com>
2021-11-10 02:09:21 +0900

    feat: dashboard 컴포넌트에 video 태그 적용

--- frontend/src/components/DashBoard/DashBoard.jsx
+++ frontend/src/components/DashBoard/DashBoard.jsx
@@ -3,20 +3,25 @@ import styled from 'styled-components';
 
 import Box from '@/components/Common/Box';
 import DashBoardInfo from './DashBoardInfo';
+import DashBoardVideo from './DashBoardVideo';
 
 export default function DashBoard({ info }) {
+    const streamKey = info.stream_key;
     return (
         <Box type="black" height="100%" alignItems="stretch">
             <Box flex={1.5} padding={1}>
                 <DashBoardInfo info={info} />
             </Box>
-            <StyledBox flex={3}>3</StyledBox>
+            <StyledBox flex={3} padding={1} alignItems="stretch">
+                <DashBoardVideo streamKey={streamKey} />
+            </StyledBox>
             <Box flex={1}>1</Box>
         </Box>
     );
 }
 
 const StyledBox = styled(Box)`
+    box-sizing: content-box;
     border-left: 0.5px solid ${({ theme }) => theme.color.gray1};
     border-right: 0.5px solid ${({ theme }) => theme.color.gray1};
 `;
