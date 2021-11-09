7d7e1c23e7a700e5e5ea531079ea068f77a4eb89
J190_정현구 <jungheun99@naver.com>
2021-11-09 17:46:59 +0900

    chore: express port 4000번으로 수정

--- backend/bin/www.js
+++ backend/bin/www.js
@@ -7,15 +7,15 @@
 import app from '../server.js';
 
 import debugFactory from 'debug';
-const debug = debugFactory("backend:server");
+const debug = debugFactory('backend:server');
 import http from 'http';
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '4000');
+app.set('port', port);
 
 /**
  * Create HTTP server.
@@ -28,8 +28,8 @@ const server = http.createServer(app);
  */
 
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -56,20 +56,20 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-    if (error.syscall !== "listen") {
+    if (error.syscall !== 'listen') {
         throw error;
     }
 
-    const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
     // handle specific listen errors with friendly messages
     switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges");
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
             process.exit(1);
             break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use");
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
             process.exit(1);
             break;
         default:
@@ -83,6 +83,7 @@ function onError(error) {
 
 function onListening() {
     const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-    debug("Listening on " + bind);
+    const bind =
+        typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 }
