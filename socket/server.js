c77758f3f0692e12fb1cc3704167fa751fbe8689
JeongwonShin <jennyshin106@gmail.com>
2021-11-09 15:13:24 +0900

    fix: handle CORS error by adding an option
    - option => cors: { origin: "*" }

--- socket/server.js
+++ socket/server.js
@@ -3,19 +3,17 @@ import { Server } from "socket.io";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  cors: { origin: "http://localhost:5000", methods: ["GET", "POST"] },
+  cors: { origin: "*" },
 });
 const port = 5000;
 
-
 io.on("connection", socket => {
-    console.log("[server] on connection");
-    socket.emit("greeting-from-server", { greeting: "Hello Client" });
-    socket.on("greeting-from-client", function (message) {
-        console.log(message);
-    });
+  console.log("[server] on connection");
+  socket.emit("greeting-from-server", { greeting: "Hello Client" });
+  socket.on("greeting-from-client", function (message) {
+    console.log(message);
+  });
 });
-
 
 httpServer.listen(port, () =>
   console.log(`Socket.io started on PORT: ${port}`)
