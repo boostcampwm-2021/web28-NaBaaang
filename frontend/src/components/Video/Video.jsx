fabb0f4dba8bf37f1d96b6f7deffb765673819d9
J190_정현구 <jungheun99@naver.com>
2021-11-09 14:19:34 +0900

    refactor: 실시간 스트리밍 비디어 스토리 수정

--- frontend/src/components/Video/Video.jsx
+++ frontend/src/components/Video/Video.jsx
@@ -5,31 +5,34 @@ import Hls from 'hls.js/dist/hls';
 import styled from 'styled-components';
 import { sizeMixin } from '@/styles/mixins';
 
-export default function Video({ videoSrc }) {
+export default function Video({ secretKey }) {
     const videoRef = useRef();
 
     useEffect(() => {
         if (Hls.isSupported()) {
             const hls = new Hls();
+
             // bind them together
             hls.attachMedia(videoRef.current);
+
             // MEDIA_ATTACHED event is fired by hls object once MediaSource is ready
-            hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-                console.log('video and hls.js are now bound together !');
-                hls.loadSource('http://localhost:5959/images/output.m3u8');
-                hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-                    console.log(
-                        `manifest loaded, found ${data.levels.length} quality level`,
-                    );
+            hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+                const serverLocation = 'http://localhost:5959';
+                const path = 'images';
+                const fileName = `${secretKey}.m3u8`;
+                const url = `${serverLocation}/${path}/${fileName}`;
+
+                hls.loadSource(url);
+
+                hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                    videoRef.current.play();
                 });
             });
         }
     }, []);
 
     return Hls.isSupported() ? (
-        <StyledVideo controls autoplay ref={videoRef}>
-            <source src={videoSrc} />
-        </StyledVideo>
+        <StyledVideo controls autoplay muted ref={videoRef} />
     ) : (
         <div>스트리밍을 지원하지 않습니다.</div>
     );
