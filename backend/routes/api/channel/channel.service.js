3633e59461e85360baf868350e66c063f3529a8a
jihoho <jihoho4589@gmail.com>
2021-11-10 06:11:09 +0900

    refactor: channel service에서 dao 호출 parameter 변경

--- backend/routes/api/channel/channel.service.js
+++ backend/routes/api/channel/channel.service.js
@@ -7,10 +7,7 @@ const create = async channelInfo => {
     try {
         const stream_key = v4();
         channelInfo.stream_key = stream_key;
-        const result = await channelDAO.insertChannel({
-            channelInfo,
-            transaction,
-        });
+        const result = await channelDAO.insertChannel(channelInfo, transaction);
 
         await transaction.commit();
         return result;
@@ -23,10 +20,7 @@ const create = async channelInfo => {
 const getChannelById = async channelId => {
     const transaction = await db.sequelize.transaction();
     try {
-        const result = await channelDAO.findByChannelId({
-            channelId,
-            transaction,
-        });
+        const result = await channelDAO.findByChannelId(channelId, transaction);
 
         await transaction.commit();
         return result;
