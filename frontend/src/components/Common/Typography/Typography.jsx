8414e4cc956b8c49dd7f9ccdbe499f696dc5581f
Jaeuk Lim <applewodnr@naver.com>
2021-11-10 01:19:41 +0900

    fix: Typography 컴포넌트의 styled tag가 함수 안에서 작성해서 발생하는 오류 해결

--- frontend/src/components/Common/Typography/Typography.jsx
+++ frontend/src/components/Common/Typography/Typography.jsx
@@ -1,42 +1,73 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export default function Typography(props) {
-    const StyledText = generateDynamicallyStyledTag(props);
-    const { children } = props;
-    return <StyledText>{children}</StyledText>;
+export default function Typography({
+    variant = 'h6',
+    color = 'black',
+    align = 'left',
+    children = '',
+}) {
+    const styleProps = { variant, color, align };
+    switch (variant) {
+        case 'h1':
+            return <H1Tag {...styleProps}>{children}</H1Tag>;
+        case 'h2':
+            return <H2Tag {...styleProps}>{children}</H2Tag>;
+        case 'h3':
+            return <H3Tag {...styleProps}>{children}</H3Tag>;
+        case 'h4':
+            return <H4Tag {...styleProps}>{children}</H4Tag>;
+        case 'h5':
+            return <H5Tag {...styleProps}>{children}</H5Tag>;
+        case 'h6':
+            return <H6Tag {...styleProps}>{children}</H6Tag>;
+        case 'span':
+            return <SpanTag {...styleProps}>{children}</SpanTag>;
+        case 'p':
+            return <PTag {...styleProps}>{children}</PTag>;
+        default:
+            return null;
+    }
 }
 
-const VariantType = {
-    h1: styled.h1`
+const FontSizeType = {
+    h1: css`
         font-size: 2rem;
     `,
-    h2: styled.h2`
+    h2: css`
         font-size: 1.8rem;
     `,
-    h3: styled.h3`
+    h3: css`
         font-size: 1.6rem;
     `,
-    h4: styled.h4`
+    h4: css`
         font-size: 1.4rem;
     `,
-    h5: styled.h5`
+    h5: css`
         font-size: 1.2rem;
     `,
-    h6: styled.h6`
+    h6: css`
         font-size: 1rem;
     `,
-    p: styled.p`
+    p: css`
         font-size: 1rem;
     `,
-    span: styled.span`
+    span: css`
         font-size: 1rem;
     `,
 };
 
-function generateDynamicallyStyledTag({ variant = 'h6', align, color }) {
-    return styled(VariantType[variant])`
-        text-align: ${align};
-        color: ${color};
-    `;
-}
+const generatorTag = variant => styled(variant)`
+    ${({ variant }) => FontSizeType[variant]};
+    color: ${({ color }) => color};
+    text-align: ${({ align }) => align};
+`;
+
+const H1Tag = generatorTag('h1');
+const H2Tag = generatorTag('h2');
+const H3Tag = generatorTag('h3');
+const H4Tag = generatorTag('h4');
+const H5Tag = generatorTag('h5');
+const H6Tag = generatorTag('h6');
+const SpanTag = generatorTag('span');
+const PTag = generatorTag('p');
