dbc06e3af42594fc3bde57eab8e77325a864057f
J190_정현구 <jungheun99@naver.com>
2021-11-09 12:39:03 +0900

    feat: hls.js와 video태그 연동
    - hls.js 추가
    - 임시로 만든 express로 테스트함.

--- frontend/src/components/Video/Video.jsx
+++ frontend/src/components/Video/Video.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+
+import Hls from 'hls.js/dist/hls';
+
 import styled from 'styled-components';
 import { sizeMixin } from '@/styles/mixins';
 
 export default function Video({ videoSrc }) {
-    return (
-        <StyledVideo controls autoplay>
+    const videoRef = useRef();
+
+    useEffect(() => {
+        if (Hls.isSupported()) {
+            const hls = new Hls();
+            // bind them together
+            hls.attachMedia(videoRef.current);
+            // MEDIA_ATTACHED event is fired by hls object once MediaSource is ready
+            hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+                console.log('video and hls.js are now bound together !');
+                hls.loadSource('http://localhost:5959/images/output.m3u8');
+                hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+                    console.log(
+                        `manifest loaded, found ${data.levels.length} quality level`,
+                    );
+                });
+            });
+        }
+    }, []);
+
+    return Hls.isSupported() ? (
+        <StyledVideo controls autoplay ref={videoRef}>
             <source src={videoSrc} />
         </StyledVideo>
+    ) : (
+        <div>스트리밍을 지원하지 않습니다.</div>
     );
 }
 
