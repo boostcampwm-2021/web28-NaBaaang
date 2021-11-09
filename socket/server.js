b8217be19afc2e81a382b62e3b9d78419f4db94e
JeongwonShin <jennyshin106@gmail.com>
2021-11-09 14:40:36 +0900

    feat: create socket server listening on port 5000

--- socket/server.js
+++ socket/server.js
@@ -1,2 +1,22 @@
-const http = require("http");
+import { createServer } from "http";
+import { Server } from "socket.io";
 
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: { origin: "http://localhost:5000", methods: ["GET", "POST"] },
+});
+const port = 5000;
+
+
+io.on("connection", socket => {
+    console.log("[server] on connection");
+    socket.emit("greeting-from-server", { greeting: "Hello Client" });
+    socket.on("greeting-from-client", function (message) {
+        console.log(message);
+    });
+});
+
+
+httpServer.listen(port, () =>
+  console.log(`Socket.io started on PORT: ${port}`)
+);
