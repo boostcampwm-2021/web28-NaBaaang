f113fe3c493b9dfc7c3559e176cfb583d68b5058
JeongwonShin <jennyshin106@gmail.com>
2021-11-10 19:12:33 +0900

    feat: create socket namespace for chatting
    - REACT_APP_SOCKET_HOST env 변수를 추가하여 활용

--- frontend/src/components/Socket/SocketTest.jsx
+++ frontend/src/components/Socket/SocketTest.jsx
@@ -1,17 +1,33 @@
 import React, { useEffect } from 'react';
 import io from 'socket.io-client';
 
-const BASE_URL = 'http://localhost:5000/';
+require('dotenv').config();
+
+const BASE_URL = process.env.REACT_APP_SOCKET_HOST;
+
+const createChatSocket = () => {
+    const chatSocket = io(`${BASE_URL}/chat`);
+
+    chatSocket.on('connect', () => {
+        // chatSocket.emit('join', { roomId: 'some random key' });
+        chatSocket.on('join', msg => console.log(msg));
+        // chatSocket.emit('chat', { message: 'Hello Server' });
+        chatSocket.on('chat', msg => console.log(msg));
+    });
+
+    chatSocket.on('disconnect', () => {
+        chatSocket.removeAllListeners();
+    });
+    return chatSocket;
+};
 
 const SocketTest = () => {
-    const chatSocket = io(BASE_URL);
+    const chatSocket = createChatSocket();
     useEffect(() => {
         console.log('socket test');
         chatSocket.on('connect', () => {
             chatSocket.emit('join', { roomId: 'some random key' });
-            chatSocket.on('after-join', msg => console.log(msg));
-            chatSocket.emit('greeting-from-client', 'Hello Server');
-            chatSocket.on('greeting-from-server', msg => console.log(msg));
+            chatSocket.emit('chat', { message: 'Hello Server' });
         });
     }, []);
     return <></>;
