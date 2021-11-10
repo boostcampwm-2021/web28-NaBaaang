7def845edc1ae754a1d30e8a47f3a8b18cc55a7f
JeongwonShin <jennyshin106@gmail.com>
2021-11-10 22:55:16 +0900

    fix: Video Overlay 삭제

--- frontend/src/components/Video/Video.jsx
+++ frontend/src/components/Video/Video.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import HLS from 'hls.js/dist/hls';
 
@@ -6,12 +6,10 @@ import { sizeMixin } from '@/styles/mixins';
 import { MEDIA_URL } from '@/constants/url';
 
 import Box from '@/components/Common/Box';
-import VideoOverlay from '@/components/VideoOverlay';
 
 export default function Video({ streamKey }) {
     const videoRef = useRef();
     const m3u8URL = `${MEDIA_URL}/${streamKey}.m3u8`;
-    const [loading, setLoading] = useState(false);
 
     const hls = new HLS();
 
@@ -24,12 +22,10 @@ export default function Video({ streamKey }) {
             });
 
             hls.on(HLS.Events.ERROR, () => {
-                if (!loading) setLoading(true);
                 hls.loadSource(m3u8URL);
             });
 
             hls.on(HLS.Events.MANIFEST_PARSED, () => {
-                if (loading) setLoading(false);
                 videoRef.current.play();
             });
         }
@@ -37,7 +33,6 @@ export default function Video({ streamKey }) {
 
     return HLS.isSupported() ? (
         <Box width="100%" height="100%">
-            {loading && <VideoOverlay open={loading} />}
             <StyledVideo controls autoplay muted ref={videoRef} />
         </Box>
     ) : (
