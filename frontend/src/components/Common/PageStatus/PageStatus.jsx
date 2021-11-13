2e0cda2444963e7cdf1182408413b88eb8d49a38
JeongwonShin <jennyshin106@gmail.com>
2021-11-14 02:25:19 +0900

    refactor: PageStatus 컴포넌트 내 중복코드 StatusBox로 처리

--- frontend/src/components/Common/PageStatus/PageStatus.jsx
+++ frontend/src/components/Common/PageStatus/PageStatus.jsx
@@ -4,31 +4,19 @@ import Box from '../Box/Box';
 import Typography from '../Typography/Typography';
 
 function PageStatus({ loading, error, data }) {
-    if (loading)
-        return (
-            <Box flexDirection="column" width="100%" height="100%">
-                <Typography variant="h3" align="center">
-                    LOADING...
-                </Typography>
-            </Box>
-        );
-    if (error)
-        return (
-            <Box flexDirection="column" width="100%" height="100%">
-                <Typography variant="h3" align="center">
-                    FETCH ERROR...
-                </Typography>
-            </Box>
-        );
-    if (!data)
-        return (
-            <Box flexDirection="column" width="100%" height="100%">
-                <Typography variant="h3" align="center">
-                    EMPTY DATA...
-                </Typography>
-            </Box>
-        );
+    if (loading) return StatusBox('LOADING...');
+    if (error) return StatusBox('FETCH ERROR...');
+    if (!data) return StatusBox('EMPTY DATA...');
 }
 
+const StatusBox = text => {
+    return (
+        <Box flexDirection="column" width="100%" height="100%">
+            <Typography variant="h3" align="center">
+                {text}
+            </Typography>
+        </Box>
+    );
+};
 
 export default PageStatus;
