4f2230ba6c5000edc7b1ecaf91ff79af7dcdb926
JeongwonShin <jennyshin106@gmail.com>
2021-11-14 01:21:32 +0900

    refactor: fetchAction switch문 제거 & actionTypeInfo 추가

--- frontend/src/constants/fetchAction.js
+++ frontend/src/constants/fetchAction.js
@@ -1,6 +1,6 @@
 import { API_URL, MEDIA_URL } from '@/constants/url';
 
-const fetchTemplate = (method, payload='') => {
+const fetchTemplate = (method, payload = '') => {
     const template = {
         option: {
             method,
@@ -19,19 +19,19 @@ const actionTypeInfo = payload => {
     return {
         FETCH_CREATE_CHANNEL: {
             url: `${API_URL}/api/channels`,
-            ...fetchTemplate('POST', )
+            ...fetchTemplate('POST', payload),
         },
         FETCH_GET_CHANNEL: {
             url: `${API_URL}/api/channels/${payload}`,
-            method: 'GET',
+            ...fetchTemplate('GET'),
         },
         FETCH_OPEN_CHANNEL: {
             url: `${API_URL}/api/channels/${payload}/open`,
-            method: 'PATCH',
+            ...fetchTemplate('PATCH'),
         },
         FETCH_CLOSE_CHANNEL: {
             url: `${API_URL}/api/channels/${payload}/close`,
-            method: 'PATCH',
+            ...fetchTemplate('PATCH'),
         },
         FETCH_READY_MEDIA: {
             url: `${MEDIA_URL}/${payload}.m3u8`,
@@ -43,56 +43,6 @@ const actionTypeInfo = payload => {
 };
 
 export default function fetchAction({ type, payload }) {
-    switch (type) {
-        case 'FETCH_CREATE_CHANNEL':
-            return {
-                url: `${API_URL}/api/channels`,
-                option: {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json;charset=UTF-8',
-                    },
-                    body: JSON.stringify(payload),
-                },
-            };
-        case 'FETCH_GET_CHANNEL':
-            return {
-                url: `${API_URL}/api/channels/${payload}`,
-                option: {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json;charset=UTF-8',
-                    },
-                },
-            };
-        case 'FETCH_OPEN_CHANNEL':
-            return {
-                url: `${API_URL}/api/channels/${payload}/open`,
-                option: {
-                    method: 'PATCH',
-                    headers: {
-                        'Content-Type': 'application/json;charset=UTF-8',
-                    },
-                },
-            };
-        case 'FETCH_CLOSE_CHANNEL':
-            return {
-                url: `${API_URL}/api/channels/${payload}/close`,
-                option: {
-                    method: 'PATCH',
-                    headers: {
-                        'Content-Type': 'application/json;charset=UTF-8',
-                    },
-                },
-            };
-        case 'FETCH_READY_MEDIA':
-            return {
-                url: `${MEDIA_URL}/${payload}.m3u8`,
-                option: {
-                    method: 'HEAD',
-                },
-            };
-        default:
-            return '';
-    }
+    const actionType = actionTypeInfo(payload);
+    return actionType[type];
 }
