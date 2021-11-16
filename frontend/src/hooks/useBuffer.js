90bc5a6f8a79e93e755bba398749633e63145ea8
Jaeuk Lim <applewodnr@naver.com>
2021-11-16 19:16:55 +0900

    refactor: modify useBuffer method

--- frontend/src/hooks/useBuffer.js
+++ frontend/src/hooks/useBuffer.js
@@ -4,31 +4,16 @@ import ArrayBuffer from '@/util/buffer';
 export default function useBuffer(size) {
     const buffer = useRef(new ArrayBuffer(size));
 
-    const isBufferFull = () => {
-        return buffer.current.isFull();
-    };
-
-    const flushBuffer = () => {
-        return buffer.current.flush();
-    };
-
-    const pushBuffer = data => {
-        buffer.current.push(data);
-    };
-
-    const getBufferList = () => {
-        return buffer.current.get();
-    };
-
-    const getBufferLength = () => {
-        return buffer.current.length();
+    const fn = {
+        isBufferFull: () => buffer.current.isFull(),
+        flushBuffer: () => buffer.current.flush(),
+        pushBuffer: data => buffer.current.push(data),
+        getBufferList: () => buffer.current.get(),
+        getBufferLength: () => buffer.current.length(),
     };
 
     return {
-        isBufferFull,
-        flushBuffer,
-        getBufferList,
-        getBufferLength,
-        pushBuffer,
+        buffer,
+        ...fn,
     };
 }
