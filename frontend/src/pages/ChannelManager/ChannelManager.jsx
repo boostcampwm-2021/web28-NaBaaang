ddd7adad0b21e8cd7ccde95941c98d2816df3e88
jihoho <jihoho4589@gmail.com>
2021-11-17 21:45:31 +0900

    fix: react  match 제거

--- frontend/src/pages/ChannelManager/ChannelManager.jsx
+++ frontend/src/pages/ChannelManager/ChannelManager.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import { useParams } from 'react-router-dom';
 import useFetch from '@/hooks/useFetch';
 
 import DashBoard from '@/components/DashBoard';
 import PageStatus from '@/components/Common/PageStatus';
 
-export default function ChannelManager({ match }) {
-    const { params } = match;
+export default function ChannelManager() {
+    const params = useParams();
     const { channelId } = params;
     const { data, loading, error } = useFetch({
         type: 'FETCH_GET_CHANNEL',
