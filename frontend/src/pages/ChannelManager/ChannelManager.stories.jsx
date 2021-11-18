ee39912925c9c20c2adc58c0951f36f0987ccea9
jihoho <jihoho4589@gmail.com>
2021-11-18 23:32:18 +0900

    fix: useSocket 라우팅 시점에 호출하도록 수정

--- frontend/src/pages/ChannelManager/ChannelManager.stories.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ChannelManager from '@/pages/ChannelManager';
-
-storiesOf('ChannelManager', module).add('default', () => <ChannelManager />);
