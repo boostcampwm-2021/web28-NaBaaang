12fcaf0770eff1108cea63b468d89fc2a28d434d
Jaeuk Lim <applewodnr@naver.com>
2021-11-23 01:46:22 +0900

    feat: add constants for OBSModal

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,26 @@
+const OBS_DESCRIPTION_LIST = [
+    {
+        id:0,
+        title: 'Step 1',
+        subTitle:'송출 정보 확인 모달을 통해 서버 URL, 스트림 키를 확인해주세요.'
+
+    },
+    {
+        id:1,
+        title: 'Step 2',
+        subTitle:'OBS Studio를 설치해주세요.'
+
+    },
+    {
+        id:2,
+        title: 'Step 3',
+        subTitle:'OBS Studio를 실행 후 서버, 스트림키를 입력해주세요.'
+    },
+    {
+        id:3,
+        title: 'Step 4',
+        subTitle:'OBS Studio에서 방송 시작 버튼을 클릭해주세요.'
+    },
+]
+
+export {OBS_DESCRIPTION_LIST}
