7681389bdfa69496a4b2da73130d30ebc83ecdee
Jaeuk Lim <applewodnr@naver.com>
2021-11-22 15:51:16 +0900

    design: add button reset css

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -26,6 +26,12 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
+  button{
+    outline: none;
+    border: none;
+    background: none;
+  }
+    
   
 
   ${fontMixin('notoSansLight', notoSansLight)}
