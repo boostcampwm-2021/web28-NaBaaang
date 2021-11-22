7a624c063e4e8571b0c596388e59fe0e6f3b2a6c
jihoho <jihoho4589@gmail.com>
2021-11-23 00:14:35 +0900

    docs: 채널 API Swagger 작성
    - 기존 @swagger 주석 작성이 아닌 yaml 파일로  분리
    - swagge-cli bundle 명령어로 분리된 yaml 파일 병합

--- backend/routes/api/channel/index.js
+++ backend/routes/api/channel/index.js
@@ -2,23 +2,7 @@ import express from 'express';
 const router = express.Router();
 import channelController from './channel.controller.js';
 import authController from '../auth/auth.controller.js';
-/**
- * @swagger
- *
- * /user:
- *   get:
- *     tags:
- *       - pet
- *     description: pet id로 정보 가져오기
- *     produces:
- *       - application/json
- *     parameters:
- *       - $ref: '#/components/parameters/path/id'
- *     responses:
- *       200:
- *         $ref: '#/components/responses/get/200/pet'
- *
- */
+
 router.get('/:id', channelController.getChannel);
 router.get(
     '/:id/authenticate',
