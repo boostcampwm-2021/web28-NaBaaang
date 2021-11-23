98da3495f9d68343b6e0134035d85ac7531f2b9c
jihoho <jihoho4589@gmail.com>
2021-11-23 11:44:10 +0900

    fix: 채팅창에서 로그인 alert modal 활성화 안되는 오류 해결

--- frontend/src/components/Chat/LoginAlertModal/LoginAlertModal.jsx
+++ frontend/src/components/Chat/LoginAlertModal/LoginAlertModal.jsx
@@ -4,7 +4,7 @@ import { Modal, Typography } from '@/components/Common';
 
 function LoginAlertModal({ onSuccess }) {
     return (
-        <Modal open onSuccessText="확인" onSuccess={onSuccess}>
+        <Modal open successText="확인" onSuccess={onSuccess}>
             <Typography variant="p">로그인을 해주세요.</Typography>
         </Modal>
     );
