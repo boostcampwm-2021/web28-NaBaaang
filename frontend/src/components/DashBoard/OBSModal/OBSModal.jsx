435685ad9d401f3830fee07a69d2105b818fcf57
Jaeuk Lim <applewodnr@naver.com>
2021-11-24 23:58:36 +0900

    feat: add image, link in OBSModal

--- frontend/src/components/DashBoard/OBSModal/OBSModal.jsx
+++ frontend/src/components/DashBoard/OBSModal/OBSModal.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 
 import { OBS_DESCRIPTION_LIST } from '@/constants';
 
-import {
-    Slider,
-    SliderItem,
-    Typography,
-    Box,
-} from '@/components/Common';
+import { Slider, SliderItem, Typography, Box } from '@/components/Common';
+
+const parseContent = contents =>
+    contents.map(content => (
+        <Typography key={content.id} variant="h4" marginBottom={1} align="left">
+            {content.id + 1}.{content.text}
+        </Typography>
+    ));
+
+const parseImages = images =>
+    images.map(image => (
+        <SliderItem color="success" key={image.id}>
+            <img width="100%" src={image.src} alt="" />
+        </SliderItem>
+    ));
+
+const paresLinks = links =>
+    links.map(link => (
+        <a key={link.id} href={link.url}>
+            {link.text}
+        </a>
+    ));
 
 export default function OBSModal() {
     const slierItemList = OBS_DESCRIPTION_LIST.map(
-        ({ id, title, subTitle }) => (
+        ({ id, title, contents, images, links }) => (
             <SliderItem key={id} padding={3}>
                 <Box
                     flex={1}
@@ -22,12 +38,14 @@ export default function OBSModal() {
                     <Typography variant="h2" marginBottom={2} align="left">
                         {title}
                     </Typography>
-                    <Typography variant="h4" marginBottom={1} align="left">
-                        {subTitle}
-                    </Typography>
+
+                    {parseContent(contents)}
+                    {paresLinks(links)}
                 </Box>
 
-                <Box flex={3}>이미지</Box>
+                <Box flex={2}>
+                    <Slider navigation>{parseImages(images)}</Slider>
+                </Box>
             </SliderItem>
         ),
     );
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,66 +1,96 @@
-import { ReactComponent as OBS0 } from '@/assets/images/OBS0.png';
-import { ReactComponent as OBS1 } from '@/assets/images/OBS1.png';
-import { ReactComponent as OBS2 } from '@/assets/images/OBS2.png';
-import { ReactComponent as OBS3 } from '@/assets/images/OBS3.png';
-import { ReactComponent as OBS4 } from '@/assets/images/OBS4.png';
-import { ReactComponent as OBS5 } from '@/assets/images/OBS5.png';
-import { ReactComponent as OBS6 } from '@/assets/images/OBS6.png';
-import { ReactComponent as OBS7 } from '@/assets/images/OBS7.png';
+import OBS1_1 from '@/assets/images/OBS1-1.png';
+import OBS2_1 from '@/assets/images/OBS2-1.png';
+import OBS3_1 from '@/assets/images/OBS3-1.png';
+import OBS3_2 from '@/assets/images/OBS3-2.png';
+import OBS3_3 from '@/assets/images/OBS3-3.png';
+import OBS4_1 from '@/assets/images/OBS4-1.png';
+import OBS5_1 from '@/assets/images/OBS5-1.png';
+import OBS6_1 from '@/assets/images/OBS6-1.png';
 
 const OBS_DESCRIPTION_LIST = [
     {
         id: 0,
         title: 'Step 1',
-        content: [
-            '송출 정보 확인 탭을 눌러 스트림 키랑 서버 URL을 확인하세요.',
+        contents: [
+            {
+                id: 0,
+                text: '송출 정보 확인 탭을 눌러 스트림 키랑 서버 URL을 확인하세요.',
+            },
         ],
         links: [],
-        images: [OBS0],
+        images: [{ id: 0, src: OBS1_1 }],
     },
     {
         id: 1,
         title: 'Step 2',
-        content: ['아래 링크에서 OBS Studio를 설치해 주세요.'],
-        links: [
-            { text: 'OBS 다운로드', url: 'https://obsproject.com/ko/download' },
+        contents: [
+            { id: 0, text: '아래 링크에서 OBS Studio를 설치해 주세요.' },
         ],
-        images: [OBS1],
+        links: [
+            {
+                id: 0,
+                text: 'OBS 다운로드',
+                url: 'https://obsproject.com/ko/download',
+            },
+        ],
+        images: [{ id: 0, src: OBS2_1 }],
     },
     {
         id: 2,
         title: 'Step 3',
-        content: [
-            'OBS Studio를 실행 후 방송을  설정해주세요.',
-            'Step1에서 확인한 서버 URL(media-url) 스트림 키(stream-key)를 입력해주세요.',
-            '영상 송출을 위해 OBS의 방송 시작 버튼을 클릭해 주세요.',
+        contents: [
+            { id: 0, text: 'OBS Studio를 실행 후 방송을  설정해주세요.' },
+            {
+                id: 1,
+                text: 'Step1에서 확인한 서버 URL(media-url) 스트림 키(stream-key)를 입력해주세요.',
+            },
+            {
+                id: 2,
+                text: '영상 송출을 위해 OBS의 방송 시작 버튼을 클릭해 주세요.',
+            },
         ],
         links: [],
-        images: [OBS2, OBS3, OBS4],
+        images: [
+            { id: 0, src: OBS3_1 },
+            { id: 1, src: OBS3_2 },
+            { id: 2, src: OBS3_3 },
+        ],
     },
     {
         id: 3,
         title: 'Step 4',
-        content: ['방송 공개를 위해 웹페이지의 방송 시작 버튼을 눌러주세요.'],
+        contents: [
+            {
+                id: 0,
+                text: '방송 공개를 위해 웹페이지의 방송 시작 버튼을 눌러주세요.',
+            },
+        ],
         links: [],
-        images: [OBS5],
+        images: [{ id: 0, src: OBS4_1 }],
     },
     {
         id: 4,
         title: 'Step 5',
-        content: [
-            '방송 종료를 원할 경우 방송 관리 페이지의 방송 종료 버튼을 클릭해주세요.',
+        contents: [
+            {
+                id: 0,
+                text: '방송 종료를 원할 경우 방송 관리 페이지의 방송 종료 버튼을 클릭해주세요.',
+            },
         ],
         links: [],
-        images: [OBS6],
+        images: [{ id: 0, src: OBS5_1 }],
     },
     {
         id: 5,
         title: 'Step 6',
-        content: [
-            '방송 종료를 원할 경우 방송 관리 페이지의 방송 종료 버튼을 클릭해주세요.',
+        contents: [
+            {
+                id: 0,
+                text: '방송 종료를 원할 경우 방송 관리 페이지의 방송 종료 버튼을 클릭해주세요.',
+            },
         ],
         links: [],
-        images: [OBS7],
+        images: [{ id: 0, src: OBS6_1 }],
     },
 ];
 
