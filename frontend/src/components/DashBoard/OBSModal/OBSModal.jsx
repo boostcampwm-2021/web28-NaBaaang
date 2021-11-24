780114d7bf40de29a5c01746e3ddbc428683ab89
Jaeuk Lim <applewodnr@naver.com>
2021-11-25 00:22:38 +0900

    refactor: OBSModal에서 render을 위한 함수를 분리

--- frontend/src/components/DashBoard/OBSModal/OBSModal.jsx
+++ frontend/src/components/DashBoard/OBSModal/OBSModal.jsx
@@ -1,58 +1,58 @@
 import React from 'react';
 
 import { OBS_DESCRIPTION_LIST } from '@/constants';
-
 import { Slider, SliderItem, Typography, Box } from '@/components/Common';
 
-const parseContent = contents =>
+export default function OBSModal() {
+    return (
+        <Box width="60rem" height="42rem">
+            <Slider navigation>{renderItems}</Slider>
+        </Box>
+    );
+}
+
+const renderContents = contents =>
     contents.map(content => (
         <Typography key={content.id} variant="h4" marginBottom={1} align="left">
             {content.id + 1}.{content.text}
         </Typography>
     ));
 
-const parseImages = images =>
+const renderImages = images =>
     images.map(image => (
         <SliderItem key={image.id}>
             <img width="100%" src={image.src} alt="" />
         </SliderItem>
     ));
 
-const paresLinks = links =>
+const renderLinks = links =>
     links.map(link => (
         <a key={link.id} href={link.url}>
             {link.text}
         </a>
     ));
 
-export default function OBSModal() {
-    const slierItemList = OBS_DESCRIPTION_LIST.map(
-        ({ id, title, contents, links, images }) => (
-            <SliderItem key={id} padding={3}>
-                <Box
-                    flex={1}
-                    flexDirection="column"
-                    justifyContent="flex-start"
-                    alignItems="flex-start"
-                >
-                    <Typography variant="h2" marginBottom={2} align="left">
-                        {title}
-                    </Typography>
+const renderItems = OBS_DESCRIPTION_LIST.map(
+    ({ id, title, contents, links, images }) => (
+        <SliderItem key={id} padding={3}>
+            <Box
+                flex={1}
+                flexDirection="column"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+                marginRight={1}
+            >
+                <Typography variant="h2" marginBottom={2} align="left">
+                    {title}
+                </Typography>
 
-                    {parseContent(contents)}
-                    {paresLinks(links)}
-                </Box>
+                {renderContents(contents)}
+                {renderLinks(links)}
+            </Box>
 
-                <Box flex={2}>
-                    <Slider navigation>{parseImages(images)}</Slider>
-                </Box>
-            </SliderItem>
-        ),
-    );
-
-    return (
-        <Box width="60rem" height="42rem">
-            <Slider navigation>{slierItemList}</Slider>
-        </Box>
-    );
-}
+            <Box flex={2}>
+                <Slider navigation>{renderImages(images)}</Slider>
+            </Box>
+        </SliderItem>
+    ),
+);
