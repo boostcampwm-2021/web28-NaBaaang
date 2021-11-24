3fa7a0053047849fbb23619ed5e75318ac260935
jihoho <jihoho4589@gmail.com>
2021-11-24 18:11:58 +0900

    refactor: [FE] DashBoardInfo 컴포넌트 방송 업데이트 핸들러 및  방송 from modal 활성화 기능 구현

--- frontend/src/components/DashBoard/DashBoardInfo/DashBoardInfo.jsx
+++ frontend/src/components/DashBoard/DashBoardInfo/DashBoardInfo.jsx
@@ -1,15 +1,29 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { ModalContext } from '@/store/ModalStore';
 
 import { Box } from '@/components/Common';
+import ChannelModal from '@/components/Header/ChannelModal';
+import { fetchUpdateChannel } from '@/apis/channel';
 import DashBoardCard from '../DashBoardCard';
 import MediaInfoModal from '../MediaInfoModal';
 import OBSModal from '../OBSModal';
 
 export default function DashBoardInfo({ info }) {
-    const { streamKey } = info;
+    const { id, title, description, category, streamKey } = info;
     const { openModal } = useContext(ModalContext);
+
+    const navigate = useNavigate();
+
+    const handleOnUpdateChannel = async formData => {
+        try {
+            const { id } = await fetchUpdateChannel(formData);
+            navigate(`/stream-manager/${id}`);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 
     const openMedialModal = () => {
         openModal(<MediaInfoModal streamKey={streamKey} />);
@@ -18,6 +32,16 @@ export default function DashBoardInfo({ info }) {
     const openOBSModal = () => {
         openModal(<OBSModal />);
     };
+
+    const openChannelFormModal = () => {
+        openModal(
+            <ChannelModal
+                initFormData={{ id, title, description, category }}
+                subHandleOnSubmit={handleOnUpdateChannel}
+            />,
+        );
+    };
+
     return (
         <Box
             height="100%"
@@ -27,7 +51,11 @@ export default function DashBoardInfo({ info }) {
             alignItems="stretch"
             flex={1}
         >
-            <DashBoardCard title="방송 정보 편집" info={info} />
+            <DashBoardCard
+                onClick={openChannelFormModal}
+                title="방송 정보 편집"
+                info={info}
+            />
             <DashBoardCard onClick={openMedialModal} title="송출 정보 확인" />
             <DashBoardCard onClick={openOBSModal} title="OBS 가이드" />
         </Box>
