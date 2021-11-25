617f619730aea72b6a62ddc0e3df7af0ebaf1595
JeongwonShin <jennyshin106@gmail.com>
2021-11-25 18:13:42 +0900

    feat: show error message when input field is empty on submit

--- frontend/src/components/Header/NicknameModal/NicknameModal.jsx
+++ frontend/src/components/Header/NicknameModal/NicknameModal.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState, useContext, useRef } from 'react';
+import styled, { css } from 'styled-components';
 
-import { Box, Button, TextField, Typography } from '@/components/Common';
+import { ModalContext } from '@/store/ModalStore';
 
-function NicknameModal() {
+import { Box, Button, Typography } from '@/components/Common';
+
+function NicknameModal({ onSubmit }) {
+    const { closeModal } = useContext(ModalContext);
+    const inputRef = useRef();
+    const [error, setError] = useState(null);
+
+    const handleOnSubmit = async () => {
+        try {
+            const inputData = inputRef.current.value;
+            if (inputData === '') setError('닉네임을 입력해주세요');
+            else {
+                onSubmit(inputData);
+                closeModal();
+            }
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
+
     return (
-        <Box width="100%" height="auto">
+        <Box flexDirection="column" justifyContent="space-evenly">
             <Typography variant="h3">닉네임 변경하기</Typography>
-            <TextField value="신정원" />
-            <Button text="변경하기" color="success" />
+            <InputBox ref={inputRef} />
+            {error && <Typography variant="p">{error}</Typography>}
+            <Button text="변경하기" color="success" onClick={handleOnSubmit} />
         </Box>
     );
 }
+
+const InputBox = styled.input`
+    width: 100%;
+    height: 2rem;
+    ${({ theme }) =>
+        css`
+            background-color: ${theme.color.gray2};
+        `}
+    border-radius: 40px;
+    padding: 0.5rem;
+`;
 
 export default NicknameModal;
