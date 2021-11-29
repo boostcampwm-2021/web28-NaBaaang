bc7c46700626ce0c8bc5de20011adc22d22a196c
jihoho <jihoho4589@gmail.com>
2021-11-30 03:09:37 +0900

    refactor: [BE] DAO try catch 제거

--- backend/src/routes/api/chat/chat.dao.js
+++ backend/src/routes/api/chat/chat.dao.js
@@ -4,18 +4,14 @@ const Chat = db.chat;
 const insertChat = async (channelId, transaction) => {
     let option = {};
     if (transaction) option.transaction = transaction;
-    try {
-        const savedChat = await Chat.create(
-            {
-                channelId,
-            },
-            option,
-        );
+    const savedChat = await Chat.create(
+        {
+            channelId,
+        },
+        option,
+    );
 
-        return savedChat.id;
-    } catch (error) {
-        console.error(error);
-    }
+    return savedChat.id;
 };
 
 export default { insertChat };
