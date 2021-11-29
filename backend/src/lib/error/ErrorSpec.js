3868ed30138cd0c612ec4cb6625476eb74527a36
jihoho <jihoho4589@gmail.com>
2021-11-30 01:21:31 +0900

    feat: [BE] ErrorSpec 클래스 구현

---
+++ backend/src/lib/error/ErrorSpec.js
@@ -0,0 +1,34 @@
+export default class ErrorSpec {
+    #status;
+    #code;
+    #name;
+    #description;
+    constructor({ status, code, name, description }) {
+        console.log(status,code, name, description);
+        this.#status = status;
+        this.#code = code;
+        this.#name = name;
+        this.#description = description;
+    }
+
+    get status() {
+        return this.#status;
+    }
+    get code() {
+        return this.#code;
+    }
+    get name() {
+        return this.#name;
+    }
+    get description() {
+        return this.#description;
+    }
+
+    toObject() {
+        return {
+            code: this.#code,
+            name: this.#name,
+            description: this.#description,
+        };
+    }
+}
