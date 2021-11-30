c2cd1723ca2851cfc0f8d3c1951e223e28d5860d
jihoho <jihoho4589@gmail.com>
2021-11-30 17:46:52 +0900

    refactor: [FE] local storage handling 관련 코드 dispatch에서 처리

--- frontend/src/components/OauthCallback/GoogleAuthCallback.jsx
+++ frontend/src/components/OauthCallback/GoogleAuthCallback.jsx
@@ -2,8 +2,6 @@ import React, { useContext, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import qs from 'qs';
 
-import { setItemToLocalStorage } from '@/util';
-
 import { fetchSiginInGoogle } from '@/apis/auth';
 import { UserContext } from '@/store/UserStore';
 import { Loading } from '@/components/Common';
@@ -21,10 +19,9 @@ export default function GoogleAuthCallback() {
         try {
             const res = await fetchSiginInGoogle(code);
             const { user, accessToken, refreshToken } = res;
-            setItemToLocalStorage({ accessToken, refreshToken });
             dispatch({
                 type: 'SIGN_IN_SUCCESS',
-                payload: { user },
+                payload: { user, accessToken, refreshToken },
             });
         } catch (err) {
             dispatch({ type: 'SIGN_IN_ERROR' });
