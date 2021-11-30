5681a89e025f1d1f8960a6726dc9a9928518c3ca
J190_정현구 <jungheun99@naver.com>
2021-11-30 16:30:20 +0900

    config: 코드상에 존재하는 url, port를 환경변수로 변경

--- socket/controllers/chat.js
+++ socket/controllers/chat.js
@@ -2,41 +2,36 @@ import axios from "axios";
 import STATUS from "../constants/statusCode.js";
 import EVENT from "../constants/socketEvents.js";
 
-const API_SERVER = "http://localhost:4000";
+const API_SERVER = process.env.API_SERVER;
 
 const chat = (io, socket) => {
-  const sendChatMessage = async message => {
-    const { chatId } = socket;
-    const { userId: senderId, content } = message;
-    const storeStatus = await saveChatMessage(chatId, senderId, content);
-    console.log(message, storeStatus);
-    if (storeStatus) {
-      socket
-        .to(socket.channelId)
-        .emit(EVENT.SEND_CHAT, { ...message, status: true });
-    } else {
-      socket.emit(EVENT.SEND_CHAT, { ...message, status: false });
-    }
-  };
-
-  const saveChatMessage = async (chatId, senderId, message) => {
-    try {
-      const response = await axios.post(
-        `${API_SERVER}/api/chats/${chatId}/message`,
-        {
-          senderId,
-          content: message,
+    const sendChatMessage = async (message) => {
+        const { chatId } = socket;
+        const { userId: senderId, content } = message;
+        const storeStatus = await saveChatMessage(chatId, senderId, content);
+        console.log(message, storeStatus);
+        if (storeStatus) {
+            socket.to(socket.channelId).emit(EVENT.SEND_CHAT, { ...message, status: true });
+        } else {
+            socket.emit(EVENT.SEND_CHAT, { ...message, status: false });
         }
-      );
-      if (response.status === STATUS.CREATED) return true;
-      else return false;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  };
+    };
 
-  socket.on(EVENT.SEND_CHAT, sendChatMessage);
+    const saveChatMessage = async (chatId, senderId, message) => {
+        try {
+            const response = await axios.post(`${API_SERVER}/api/chats/${chatId}/message`, {
+                senderId,
+                content: message,
+            });
+            if (response.status === STATUS.CREATED) return true;
+            else return false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    };
+
+    socket.on(EVENT.SEND_CHAT, sendChatMessage);
 };
 
 export default chat;
