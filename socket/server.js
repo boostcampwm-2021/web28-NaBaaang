23f54ee42169317b902cf74fbea5d1d7fbd3fa7f
J190_정현구 <jungheun99@naver.com>
2021-11-30 16:30:04 +0900

    config: .env 설정을 코드에서 스크립트로 옮김
    - import 방식을 사용할 때는 node(nodemon) 옵션으로 preload하는 것을 권장함.
    - import 방식을 사용했을 때, 파일 입출력 시간 때문에 바로 적용되지 않았음.

--- socket/server.js
+++ socket/server.js
@@ -1,11 +1,7 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
-import dotenv from "dotenv";
-import path from "path";
 
 import socketLoader from "./loader.js";
-
-dotenv.config({ path: path.resolve(process.cwd(), "config/.env") });
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
