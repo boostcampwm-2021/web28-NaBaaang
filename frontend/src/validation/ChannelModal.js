99ec66f031f98ec62b4877e47182254b8bbe43d1
jihoho <jihoho4589@gmail.com>
2021-12-02 11:21:03 +0900

    refactor: [FE] ChannelFrom validation 및 error 메시지 위치 수정

--- frontend/src/validation/ChannelModal.js
+++ frontend/src/validation/ChannelModal.js
@@ -5,6 +5,10 @@ export default function validateChannelForm({ title, category, description }) {
         errors.title = '방송 제목을 입력해주세요.';
     }
 
+    if (title && title.length > 20){
+        errors.title = '방송 제목은 20자 이하로 입력해주세요.';
+    }
+
     if (!category) {
         errors.category = '방송 카테고리를 입력해주세요. ';
     }
